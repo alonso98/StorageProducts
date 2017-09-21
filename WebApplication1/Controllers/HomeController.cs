9ae605a79763f37e6ae020256a47d585af15cc26
Pol Yakov <polyakov.olexandr1998@gmail.com>
2017-09-22 00:01:06 +0300

    Добавьте файлы проекта.

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        StorageContext db = new StorageContext();
+        public ActionResult Index()
+        {
+            foreach (var item in db.Products)
+            {
+                
+            }
+            ViewBag.Products = db.Products;
+            return View();
+        }
+    }
+}
